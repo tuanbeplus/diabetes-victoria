ba7a82b12fb1e2dbdd556537a7b725fa97cc16d6
quocvietht03 <quocvietht03@gmail.com>
2024-04-15 16:18:37 +0700

    create section FAQs and Icon Promo Carousel

---
+++ modules/faqs.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying FAQs module
+ *
+ */
+
+if( get_row_layout() != 'faqs' ) {
+    return;
+}
+
+$heading = get_sub_field('heading');
+$faqs = get_sub_field('faqs');
+
+if (empty($heading) && empty($faqs)) {
+    return;
+}
+
+?>
+<section class="faqs-section">
+    <div class="container">
+        <div class="faqs-wrapper">
+            <?php if(!empty($heading)) { ?>
+                <h1 class="faqs-heading">
+                    <?php echo $heading; ?>
+                </h1>
+            <?php } ?>
+
+            <?php if(!empty($faqs)) { ?>
+                <div class="accordion js-accordion" role="tablist" aria-live="polite" data-behavior="accordion">
+                    <?php foreach($faqs as $key => $item) { ?>
+                        <div class="accordion__item js-show-item-default" data-binding="expand-accordion-item">
+                            <div id="<?php echo 'tab' . $key; ?>" tabindex="0" class="accordion__title" aria-controls="panel5" role="tab" aria-selected="false" aria-expanded="false" data-binding="expand-accordion-trigger">
+                                <h5><?php echo $item['heading']; ?></h5>
+                            </div>
+
+                            <div id="<?php echo 'panel' . $key; ?>" class="accordion__content" role="tabpanel" aria-hidden="true" aria-labelledby="tab5" data-binding="expand-accordion-container">
+                                <div class="accordion__content-inner">
+                                <?php echo $item['content']; ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</section>
--- modules/hero-carousel.php
+++ modules/hero-carousel.php
@@ -17,7 +17,7 @@ if( get_row_layout() == 'hero_carousel' ):
         ?>
         <!-- Carousel -->
         <section class="hero-carousel carousel">
-            <div id="carousel-wrapper-<?php echo $wrapper_id; ?>" class="carousel-wrapper owl-carousel">
+            <div id="carousel-wrapper-<?php echo $wrapper_id; ?>" class="carousel-wrapper carousel-wrapper-<?php echo $wrapper_id; ?>">
                 <?php foreach ($carousel_list as $id => $row): 
                     $item = $row['carousel_item'] ?? '';
                     if (!empty($item)): 
@@ -98,22 +98,20 @@ if( get_row_layout() == 'hero_carousel' ):
         </section><!-- .Carousel -->
         <script>
         jQuery(document).ready(function(){
-            let carousel_<?php echo $wrapper_id; ?> = jQuery("#carousel-wrapper-<?php echo $wrapper_id; ?>").owlCarousel({
-                loop: false,
-                margin: 0,
-                items: 1,
-                nav: false,
-                dots: <?php echo $dots; ?>,
-                autoplay: <?php echo $autoplay; ?>,
-                autoplaySpeed: <?php echo $autoplay_speed; ?>,
-                autoplayTimeout: 3000,
-                autoplayHoverPause: true,
-                navText: [
-                    '<span aria-label="Previous Slide"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></span>',
-			        '<span aria-label="Next Slide"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></span>',
-                ],
+            jQuery('.carousel-wrapper-<?php echo $wrapper_id; ?>').slick({
+                // Slick Slider options
+                infinite: false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                accessibility: true,
+                autoplay: false,
+                autoplaySpeed: 2000,
+                arrows: false,
+                dots: true,
+                dotsClass: 'slick-dots container',
+                pauseOnFocus: true,
+                pauseOnHover: true,
             });
-            jQuery("#carousel-wrapper-<?php echo $wrapper_id; ?> .owl-dots").addClass('container');
         });
         </script>
         <?php
